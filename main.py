1a4c55063addec6ba350414f50ae43854aad4da4
bebert11 <45772124+bebert11@users.noreply.github.com>
2021-09-26 12:04:45 +0200

    Update main.py

--- main.py
+++ main.py
@@ -11,4 +11,4 @@ for i in range(len(cogs)):
   cogs[i].setup(client)
 
 
-client.run("ODkxMzcwODI4NTA2MjI2Nzk5.YU9Xxg.AszKPGx6Rv5sZTqW_KEqxpTCxXM")
+client.run("ODkxMzcwODI4NTA2MjI2Nzk5.YU9Xxg.xjV9pdrGzHj96SY7MpLOVUWudp0")
